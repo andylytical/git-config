3e49f0cd52fddfe37c081fbb21b97be60dc4dd58
Andy Loftus <aloftus@gmail.com>
2019-01-25 11:26:11 -0600

    add go bash alias

--- bashrc.d/git.sh
+++ bashrc.d/git.sh
@@ -1,4 +1,5 @@
 # Alias for git diff lsst branches
 alias gdiff='git diff --color-words master...$(git rev-parse --abbrev-ref HEAD)'
 # switch branches quickly (GitGo)
-alias gg='git checkout '
+alias gg='git checkout'
+alias go='git checkout'
