c1bd6188c144b66d84b4bafec67a4de235cc2a5e
Andy Loftus <aloftus@gmail.com>
2019-01-25 09:22:14 -0600

    Update git.sh

--- bashrc.d/git.sh
+++ bashrc.d/git.sh
@@ -1,4 +1,4 @@
 # Alias for git diff lsst branches
 alias gdiff='git diff --color-words master...$(git rev-parse --abbrev-ref HEAD)'
 # switch branches quickly
-alias go='git checkout '
+alias ggo='git checkout '
