2b3e0b8c6c80f8ebfdbff3fc660fec40c0c60857
Andrew Loftus <aloftus@illinois.edu>
2019-03-04 13:01:22 -0600

    Add alias for 'repo'

--- bashrc.d/git.sh
+++ bashrc.d/git.sh
@@ -1,2 +1,5 @@
 # Alias for git diff lsst branches
 alias gdiff='git diff --color-words master...$(git rev-parse --abbrev-ref HEAD)'
+
+# Alias to quickly switch between puppet devops repos
+alias repo='cd ~/puppet; cd'
