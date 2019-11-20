44b5416653c38cc9292378f072d358c7efdcf34e
Andy Loftus <aloftus@gmail.com>
2019-11-20 12:29:39 -0600

    Add ggrep
    Exclude .git dir from recursive grep's

--- bashrc.d/git.sh
+++ bashrc.d/git.sh
@@ -1,6 +1,9 @@
 # Alias for git diff lsst branches
 alias gdiff='git diff --color-words master...$(git rev-parse --abbrev-ref HEAD)'
 
+# Exclude .git from recursive grep
+alias ggrep='grep --exclude-dir=.git'
+
 # Delete all topic branches (in current repo) that aren't mine
 alias rmtopics='git branch | grep topic | grep -vE "^\*|\/$USER\/" | xargs -n1 git branch -d'
 
