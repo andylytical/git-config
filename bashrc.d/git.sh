710fa392de82bfe40a7a2e8b4ad2297e4d65c1ce
Andy Loftus <aloftus@gmail.com>
2019-12-05 12:43:57 -0600

    repo is now a function

--- bashrc.d/git.sh
+++ bashrc.d/git.sh
@@ -7,6 +7,15 @@ alias ggrep='grep -R --exclude-dir=.git'
 # Delete all topic branches (in current repo) that aren't mine
 alias rmtopics='git branch | grep topic | grep -vE "^\*|\/$USER\/" | xargs -n1 git branch -d'
 
-# Alias to quickly switch between puppet devops repos
-# Example: "repo git-config" ... yields ...  "cd ~/working; cd git-config"
-alias repo='cd ~/working; cd'
+# quickly switch between repos; assumes a common repo directory
+repo() {
+    workdir=~/working
+    if [[ $# -lt 1 ]] ; then
+        cd "$workdir"
+    else
+        tgt="$workdir/$1"
+        [[ -d "$tgt" ]] \
+        && cd "$tgt" \
+        || echo "Invalid target '$tgt'"
+    fi
+}
