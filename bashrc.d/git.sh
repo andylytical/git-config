68f0c915b2860babc1a52015e226c48fd3c85825
Andy Loftus <aloftus@gmail.com>
2020-09-24 14:26:03 -0500

    add gprune and gpruneD

--- bashrc.d/git.sh
+++ bashrc.d/git.sh
@@ -4,8 +4,12 @@ alias gdiff='git diff --color-words master...$(git rev-parse --abbrev-ref HEAD)'
 # Exclude .git from recursive grep
 alias ggrep='grep -RI --exclude-dir=.git --exclude-dir=spec --exclude=Gemfile* --exclude=Rakefile'
 
-# Delete all topic branches (in current repo) that aren't mine
+# Delete all local topic branches (in current repo) that aren't mine
 alias rmtopics='git branch | grep topic | grep -vE "^\*|\/$USER\/" | xargs -n1 git branch -d'
+
+# Delete all local branches that don't have a corresponding remote branch
+alias gprune="git branch -r | awk '{print $1}' | egrep -v -f /dev/fd/0 <(git branch -vv | grep origin) | awk '{print $1}' | xargs git branch -d"
+alias gpruneD="git branch -r | awk '{print $1}' | egrep -v -f /dev/fd/0 <(git branch -vv | grep origin) | awk '{print $1}' | xargs git branch -D"
 
 # quickly switch between repos; assumes a common repo directory
 repo() {
